30abede303a3f48bd798f857f41aed15673482fb
uan <fspinardi08@gmail.com>
2025-08-03 15:57:22 +0200

    help messahes

--- main.go
+++ main.go
@@ -8,13 +8,20 @@ import (
 	"github.com/shlldev/miniws/miniws"
 )
 
+const (
+	HELP_PORT         string = "what port miniws will run on"
+	HELP_LOGFOLDER    string = "the logs folder"
+	HELP_CONFIGFOLDER string = "the configurations folder"
+	HELP_WWWFOLDER    string = "the www folder where miniws will look for files to serve"
+)
+
 func main() {
 	parser := argparse.NewParser("miniws", "")
 
-	port := parser.Int("p", "port", &argparse.Options{Default: 8040})
-	logFolder := parser.String("l", "logs-folder", &argparse.Options{Default: "logs"})
-	configFolder := parser.String("c", "config-folder", &argparse.Options{Default: "config"})
-	wwwFolder := parser.String("w", "www-folder", &argparse.Options{Default: "www"})
+	port := parser.Int("p", "port", &argparse.Options{Default: 8040, Help: HELP_PORT})
+	logFolder := parser.String("l", "logs-folder", &argparse.Options{Default: "logs", Help: HELP_LOGFOLDER})
+	configFolder := parser.String("c", "config-folder", &argparse.Options{Default: "config", Help: HELP_CONFIGFOLDER})
+	wwwFolder := parser.String("w", "www-folder", &argparse.Options{Default: "www", Help: HELP_WWWFOLDER})
 
 	err := parser.Parse(os.Args)
 	if err != nil {
